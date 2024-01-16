57ea5c254be1a490969af58b4c22b6539be82e99
Leon Wieser <leon.tobias.steiermark@gmail.com>
2024-01-16 11:53:11 +0100

    I think all needed libs are installed

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+# Tetris played by AI
 from nes import NES
+import pyautogui as pag;
+import sys; sys.path.append("..")
+import tensorflow as tf
+
 nes = NES("tetris.nes")
 nes.run()
+
+# Controls are:
+# Up, Left, Down, Right: W, A, S, D
+# Select, Start:  G, H
+# A, B: P, L
+
+
+
+
